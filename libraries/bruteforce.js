38bdbc0b788a66077c21eb5ec77b89d6034fc415
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2019-07-14 18:17:48 +0800

    rate limit auth route

--- libraries/bruteforce.js
+++ libraries/bruteforce.js
@@ -4,3 +4,8 @@ exports.all = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100 // 100 requests per windowMs
 });
+
+exports.auth = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 3 // 3 requests per windowMs
+});
