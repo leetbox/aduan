c7572d39dc6fb3b7d77b2dd40a90eafc04633feb
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2019-07-14 18:28:41 +0800

    complaint delete router

--- routers/complaint.js
+++ routers/complaint.js
@@ -44,3 +44,20 @@ router.patch('/:id', (req, res) => {
 		})
 	);
 });
+
+router.delete('/:id', (req, res) => {
+	const input = {};
+	input.id = req.params.id;
+
+	return complaintController
+		.delete(input)
+		.then(data => res.sendStatus(204))
+		.catch(error => res.status(400).json({
+			error: {
+				message: error.message,
+			},
+		})
+	);
+});
+
+module.exports = router;
