aaff21681e6411c01faff7a65cdd7e765b4ffadf
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2019-07-14 17:34:13 +0800

    usertype update controller

--- controllers/userType/index.js
+++ controllers/userType/index.js
@@ -60,3 +60,27 @@ exports.read = async (input) => {
       return rows.results;
     });
 }
+
+exports.update = async (input) => {
+	const {
+		id,
+		name,
+	} = input;
+
+	const u = input;
+
+	const deleted = false;
+
+	const exist = await UserType
+		.query()
+		.whereNot({ id, deleted })
+		.andWhere({ name })
+		.first();
+
+	if (!isEmpty(exist)) return Promise.reject(new Error('USER_TYPE_EXIST'));
+
+	return UserType
+		.query()
+		.where({ id, deleted })
+		.patch(u);
+};
