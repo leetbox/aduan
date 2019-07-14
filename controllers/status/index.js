6a441aabee85e0d69a61079474da647f8ac67b48
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2019-07-14 17:29:08 +0800

    read status controller

--- controllers/status/index.js
+++ controllers/status/index.js
@@ -26,3 +26,37 @@ exports.create = async (input) => {
 		.query()
 		.insert(i); // mysql don't need first
 };
+
+exports.read = async (input) => {
+	const {
+		id,
+		name,
+		page,
+		limit,
+	} = input;
+
+	if (page && page < 1) return Promise.reject(new Error('RULE_PAGE_>_0'));
+  if (limit && limit < 1) return Promise.reject(new Error('RULE_LIMIT_>_0'));
+
+  const q = {};
+  q.deleted = false;
+
+  if (id) q.id = id;
+  if (name) q.name = name;
+
+  const newLimit = parseInt(limit, 10) || pagination.limit;
+  const newPage = parseInt(page, 10) - 1 || pagination.page;
+
+  return Status
+    .query()
+    .where(q)
+    .page(newPage, newLimit)
+    .orderBy('id', 'desc')
+    .then((rows) => {
+      if (isEmpty(rows.results)) return Promise.reject(new Error('STATUS_NOT_FOUND'));
+
+      if (id) return rows.results[0];
+
+      return rows.results;
+    });
+}
