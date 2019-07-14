2906b6abee5d5f3f5d97599ccca7b9ae867e2a51
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2019-07-14 17:30:49 +0800

    read user controller

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -48,3 +48,39 @@ exports.create = async (input) => {
 		.query()
 		.insert(i); // mysql don't need first
 };
+
+exports.read = async (input) => {
+	const {
+		id,
+		page,
+		email,
+		hash,
+		limit,
+	} = input;
+
+	if (page && page < 1) return Promise.reject(new Error('RULE_PAGE_>_0'));
+  if (limit && limit < 1) return Promise.reject(new Error('RULE_LIMIT_>_0'));
+
+  const q = {};
+  q.deleted = false;
+
+  if (id) q.id = id;
+  if (email) q.email = email;
+  if (hash) q.hash = hash;
+
+  const newLimit = parseInt(limit, 10) || pagination.limit;
+  const newPage = parseInt(page, 10) - 1 || pagination.page;
+
+  return User
+    .query()
+    .where(q)
+    .page(newPage, newLimit)
+    .orderBy('id', 'desc')
+    .then((rows) => {
+      if (isEmpty(rows.results)) return Promise.reject(new Error('USER_NOT_FOUND'));
+
+      if (id) return rows.results[0];
+
+      return rows.results;
+    });
+}
